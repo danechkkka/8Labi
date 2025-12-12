44e49f4e732c36c72cf75461aaa8784d953a0547
danek <123189656+dan-nik15@users.noreply.github.com>
2025-12-12 13:44:06 +0200

    Add a main function to main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"cmp"
+	"fmt"
+	"sync"
 )
 
 func Max[T cmp.Ordered](slice []T) []T {
@@ -13,4 +15,27 @@ func Max[T cmp.Ordered](slice []T) []T {
 		}
 	}
 	return slice
+}
+
+func main() {
+	slice := make([]int, 0, 15)
+	n_slice := make([]int, 3)
+	parts := 5
+	partSize := len(slice) / parts
+	var wg sync.WaitGroup
+	for i := 0; i < len(slice); i += partSize {
+		end := i + partSize
+		if i == len(slice) {
+			end = len(slice) - 1
+		}
+		wg.Add(1)
+		go func(s []int) {
+			defer wg.Done()
+			s = Max(s)
+			n_slice = append(n_slice, s[0])
+		}(slice[i:end])
+	}
+	wg.Wait()
+	Max(n_slice)
+	fmt.Print(n_slice[0])
 }
